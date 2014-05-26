8241002619a91623414e6e51711858b923cf90da
Kei Takahashi <kei.takahashi@dena.com>
2014-05-27 01:35:37 +0900

    Update package.json

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var path = require('path');
+var gulp = require('gulp');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+var header = require('gulp-header');
+var shell = require('gulp-shell');
+var uglify = require('gulp-uglify');
+
